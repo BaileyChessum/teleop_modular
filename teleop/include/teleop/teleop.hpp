de20237d3abad69c0ba76abe646a069a2f70063c
Bailey Chessum <bailey.chessum1@gmail.com>
2025-07-05 18:16:42 +1000

    feat: refactor Teleop to be composition based, rather than use inheritance

--- teleop/include/teleop/teleop.hpp
+++ teleop/include/teleop/teleop.hpp
@@ -1,6 +1,6 @@
 /**
  * @file teleop.hpp
- * @brief Header file for the TeleopArmJoy class, which handles joystick input for teleoperation of the robotic arm.
+ * @brief Header file for the Teleop class, which handles joystick input for teleoperation of the robotic arm.
  * Last Edited by Abby
  */
 #ifndef TELEOP_HPP
@@ -13,23 +13,26 @@
 #include "control_modes/ControlModeManager.hpp"
 #include "input_sources/InputSourceManager.hpp"
 #include "commands/CommandManager.hpp"
+#include "inputs/state/StateManager.hpp"
 
 namespace teleop
 {
   /**
-   * @class TeleopArmJoy
+   * @class Teleop
    * @brief TODO
    */
-  class TeleopArmJoy : public rclcpp::Node, public CommandDelegate {
+  class Teleop : public CommandDelegate, public std::enable_shared_from_this<Teleop> {
   public:
     /**
-     * @brief Constructor for TeleopArmJoy.
+     * @brief Constructor for Teleop.
+     * @param node The ROS2 node to use for parameters, topics, and services
      * @param options Node options for the ROS2 node.
      */
-    explicit TeleopArmJoy(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
-    ~TeleopArmJoy() override;
+    explicit Teleop(const std::shared_ptr<rclcpp::Node>& node);
 
-    void initialize(const std::weak_ptr<rclcpp::Executor>& executor, const std::shared_ptr<TeleopArmJoy>& self);
+    ~Teleop() override;
+
+    void initialize(const std::weak_ptr<rclcpp::Executor>& executor);
 
     void log_all_inputs();
 
@@ -38,20 +41,32 @@ namespace teleop
      */
     [[noreturn]] void service_input_updates();
 
-    [[nodiscard]] std::shared_ptr<const Node> get_node() const override;
+    [[nodiscard]] std::shared_ptr<rclcpp::Node> get_node() const override;
     [[nodiscard]] const InputManager& get_inputs() const override;
+    [[nodiscard]] StateManager& get_states() const override;
     [[nodiscard]] const std::shared_ptr<ControlModeManager> get_control_modes() const override;
 
+    /**
+     * Ends any running threads
+     */
+    void stop();
+
   private:
+    std::shared_ptr<rclcpp::Node> node_;
+
     std::shared_ptr<ParamListener> param_listener_;
     Params params_;
 
-    std::shared_ptr<ControlModeManager> control_mode_manager_ = nullptr;
     InputManager inputs_;
+    StateManager states_;
+
+    std::shared_ptr<ControlModeManager> control_mode_manager_ = nullptr;
     std::shared_ptr<InputSourceManager> input_source_manager_ = nullptr;
     std::shared_ptr<CommandManager> commands_ = nullptr;
 
     bool locked_ = true;
+
+    std::atomic<bool> program_running_ = true;
   };
 
 } // namespace teleop
--- teleop/src/teleop.cpp
+++ teleop/src/teleop.cpp
@@ -11,38 +11,37 @@ using std::placeholders::_2;
 namespace teleop
 {
 
-TeleopArmJoy::TeleopArmJoy(const rclcpp::NodeOptions &options)
-    : Node("teleop_node", options)
+Teleop::Teleop(const std::shared_ptr<rclcpp::Node>& node)
+  : node_(node), states_(inputs_)
 {
+  // Create publishers
+  param_listener_ = std::make_shared<ParamListener>(Teleop::get_node());
+  params_ = param_listener_->get_params();
 }
 
-void TeleopArmJoy::initialize(const std::weak_ptr<rclcpp::Executor>& executor, const std::shared_ptr<TeleopArmJoy>& self) {
-  // TODO: Passing a pointer to itself is a bodge. We should refactor to a composition style, rather than inheritance.
-
-  // Create publishers
-  param_listener_ = std::make_shared<ParamListener>(shared_from_this());
-  params_ = param_listener_->get_params();
-
+void Teleop::initialize(const std::weak_ptr<rclcpp::Executor>& executor) {
   inputs_ = InputManager();
   // inputs_.get_buttons().add("locked", std::static_pointer_cast<Input<bool>>(locked_));
 
-  input_source_manager_ = std::make_shared<InputSourceManager>(shared_from_this(), executor, inputs_);
+  input_source_manager_ = std::make_shared<InputSourceManager>(get_node(), executor, inputs_);
   input_source_manager_->configure(param_listener_, inputs_);
 
-  control_mode_manager_ = std::make_shared<ControlModeManager>(shared_from_this(), executor);
+  control_mode_manager_ = std::make_shared<ControlModeManager>(get_node(), executor);
   control_mode_manager_->configure(inputs_);
 
-  commands_ = std::make_shared<CommandManager>(shared_from_this(), self);
+  commands_ = std::make_shared<CommandManager>(get_node(), shared_from_this());
   commands_->configure(inputs_);
 }
 
-void TeleopArmJoy::log_all_inputs() {
+void Teleop::log_all_inputs() {
+  const auto logger = get_node()->get_logger();
+
   for (const auto& axis : inputs_.get_axes()) {
     if (!axis)
       continue;
 
     if (axis->changed()) {
-      RCLCPP_INFO(get_logger(), C_INPUT "  %s\t%f", axis->get_name().c_str(), axis->value());
+      RCLCPP_INFO(logger, C_INPUT "  %s\t%f", axis->get_name().c_str(), axis->value());
     }
   }
   for (const auto& button : inputs_.get_buttons()) {
@@ -50,7 +49,7 @@ void TeleopArmJoy::log_all_inputs() {
       continue;
 
     if (button->changed()) {
-      RCLCPP_INFO(get_logger(), C_INPUT "  %s\t%d", button->get_name().c_str(), button->value());
+      RCLCPP_INFO(logger, C_INPUT "  %s\t%d", button->get_name().c_str(), button->value());
     }
   }
   for (auto& event : inputs_.get_events()) {
@@ -58,16 +57,16 @@ void TeleopArmJoy::log_all_inputs() {
       continue;
 
     if (event->is_invoked()) {
-      RCLCPP_INFO(get_logger(), C_QUIET "  %s invoked!", event->get_name().c_str());
+      RCLCPP_INFO(logger, C_QUIET "  %s invoked!", event->get_name().c_str());
     }
   }
 }
 
-[[noreturn]] void TeleopArmJoy::service_input_updates() {
-  rclcpp::Time previous = this->now();
+void Teleop::service_input_updates() {
+  rclcpp::Time previous = get_node()->now();
 
   // TODO: killing the thread
-  while (true) {
+  while (program_running_) {
     const auto now = input_source_manager_->wait_for_update();
 
     const auto period = now - previous;
@@ -88,19 +87,29 @@ void TeleopArmJoy::log_all_inputs() {
   }
 }
 
-std::shared_ptr<const rclcpp::Node> TeleopArmJoy::get_node() const {
-  return shared_from_this();
+std::shared_ptr<rclcpp::Node> Teleop::get_node() const {
+  return node_;
 }
 
-const InputManager& TeleopArmJoy::get_inputs() const {
+const InputManager& Teleop::get_inputs() const {
   return inputs_;
 }
 
-const std::shared_ptr<ControlModeManager> TeleopArmJoy::get_control_modes() const {
+StateManager& Teleop::get_states() const {
+  return states_;
+}
+
+const std::shared_ptr<ControlModeManager> Teleop::get_control_modes() const {
   return control_mode_manager_;
 }
 
-TeleopArmJoy::~TeleopArmJoy() {
+void Teleop::stop() {
+  program_running_ = false;
+  // TODO: Hold the thread object in the class so it can be joined here.
+}
+
+
+Teleop::~Teleop() {
   control_mode_manager_.reset();
 }
 
@@ -108,20 +117,21 @@ TeleopArmJoy::~TeleopArmJoy() {
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  const auto node = std::make_shared<teleop::TeleopArmJoy>();
+  const auto node = std::make_shared<rclcpp::Node>("teleop");
+  auto teleop = teleop::Teleop(node);
   const auto executor = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
   executor->add_node(node);
 
   std::cout << "\n";
 
-  node->initialize(executor, node);
+  teleop.initialize(executor);
 
   {
-    std::thread main_update_thread(&teleop::TeleopArmJoy::service_input_updates, node);
-    // main_update_thread.detach();
-
-    // rclcpp::spin(node);
+    std::thread main_update_thread(&teleop::Teleop::service_input_updates, &teleop);
     executor->spin();
+
+    teleop.stop();
+    main_update_thread.join();
   }
 
   rclcpp::shutdown();
