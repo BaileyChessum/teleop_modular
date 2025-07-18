b09fb329656cee891b1883bb6eb6bef84ea5b9f0
Bailey Chessum <bailey.chessum1@gmail.com>
2025-07-18 18:12:16 +1000

    feat: added default implementations to ControlMode lifecycle methods that just return with success.

--- teleop_modular/include/teleop_modular/control_modes/ControlMode.hpp
+++ teleop_modular/include/teleop_modular/control_modes/ControlMode.hpp
@@ -17,65 +17,99 @@
 namespace control_mode
 {
 
-/// To replace returning bools to indicate whether an operation was successful, or failed due to some error. Used to
-/// be more explicit. OK is false as to allow you to define functions without returning anything, and it should return
-/// OK by default.
+/*
+ * To replace returning bools to indicate whether an operation was successful, or failed due to some error. Used to
+ * be more explicit. OK is false as to allow you to define functions without returning anything, and it should return
+ * OK by default.
+ */
 enum class return_type : bool
 {
   OK = false,
   ERROR = true,
 };
 
-/// These usings are added with hopes to reduce the length of type names for implementers of control modes
+// These usings are added with hopes to reduce the length of type names for implementers of control modes
+/// The return type of LifecycleNodeInterface callback
 using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 using State = rclcpp_lifecycle::State;
 
 // Definitions may be subject to change! Please use the names in control_mode::
+/// Provides access to boolean inputs. Store as a Button::SharedPtr.
 using Button = teleop::Button;
+/// Provides access to floating point number inputs. Store as an Axis::SharedPtr.
 using Axis = teleop::Axis;
 
 // Definitions may be subject to change! Please use the names in control_mode::
+/// A set to acquire shared pointers to boolean inputs
 using ButtonCollection = teleop::InputCollection<Button>;
+/// A set to acquire shared pointers to floating point number inputs
 using AxisCollection = teleop::InputCollection<Axis>;
 
+/**
+ * @brief Provides references to all collections of input objects that might be needed in a control_mode.
+ *
+ * Including this extra struct affords abstraction for the providers of buttons, axes, and the potential expansion of
+ * available inputs (events?)
+ */
 struct Inputs {
   ButtonCollection& buttons;
   AxisCollection& axes;
 };
 
 /**
- * Base class for a control mode used in teleoperation
+ * Base class for a control mode used in teleoperation.
  */
 class ControlMode : public rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface
 {
 public:
+  /**
+   * These are parameters shared across all controllers. These can be populated by setting the control_mode_name.
+   */
   struct CommonParams {
+    /**
+     * The set of ros2_control controllers to activate with this control mode. Populated by the
+     * control_modes.control_mode_name.controllers parameter in the main teleop_modular node.
+     */
     std::vector<std::string> controllers;
   };
 
   ~ControlMode() override;
   ControlMode() = default;
 
-  /// This function effectively replaces the initializer
+  /**
+   * This function effectively replaces the initializer, and should only be called once immediately after spawning the
+   * control mode, and before anything else.
+   */
   return_type init(const std::string& name, const std::string& node_namespace,
                    const rclcpp::NodeOptions& node_options, const CommonParams& common_params);
 
   // Accessors
-  /// Name of the control mode, which the control mode is indexed by
+  /**
+   * Gets the name of the control mode, which the control mode is indexed by.
+   */
   [[nodiscard]] const std::string& get_name() const
   {
     return name_;
   }
+  /**
+   * Gets the underlying lifecycle node used by the control mode
+   */
   [[nodiscard]] const std::shared_ptr<rclcpp_lifecycle::LifecycleNode>& get_node() const
   {
     return node_;
   }
 
-  virtual return_type on_init() = 0;
+  virtual return_type on_init() { return return_type::OK; };
   virtual void capture_inputs(Inputs inputs) = 0;
   virtual return_type update(const rclcpp::Time& now, const rclcpp::Duration& period) = 0;
 
-  [[nodiscard]] const rclcpp_lifecycle::State& get_lifecycle_state() const;
+  CallbackReturn on_activate(const State&) override { return CallbackReturn::SUCCESS; };
+  CallbackReturn on_deactivate(const State&) override { return CallbackReturn::SUCCESS; };
+  CallbackReturn on_cleanup(const State&) override { return CallbackReturn::SUCCESS; };
+  CallbackReturn on_error(const State&) override { return CallbackReturn::SUCCESS; };
+  CallbackReturn on_configure(const State&) override { return CallbackReturn::SUCCESS; };
+
+  [[nodiscard]] const State& get_lifecycle_state() const;
   [[nodiscard]] const std::vector<std::string>& get_controllers() const;
 
 protected:
