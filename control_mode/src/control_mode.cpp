2c6dfc3721e36c702467d6cdb222fd0b4fdfda95
Bailey Chessum <bailey.chessum1@gmail.com>
2025-07-20 03:11:32 +1000

    fix: add even more copyright notices. holy moly

--- control_mode/src/control_mode.cpp
+++ control_mode/src/control_mode.cpp
@@ -1,5 +1,12 @@
 // Copyright 2025 Bailey Chessum
 // SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 #include "control_mode/control_mode.hpp"
 #include "lifecycle_msgs/msg/state.hpp"
 
--- teleop_core/include/teleop_core/colors.hpp
+++ teleop_core/include/teleop_core/colors.hpp
@@ -1,5 +1,12 @@
 // Copyright 2025 Bailey Chessum
 // SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 
 // Define all colors
 #define C_TITLE "\033[1;94m"
