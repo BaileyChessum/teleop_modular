1a6153b7ae1c151be6441037117fd000d776d7e8
Bailey Chessum <bailey.chessum1@gmail.com>
2026-05-17 17:10:38 +1000

    feat: an empty control mode, to do nothing. (#61)

--- control_mode_plugins/default.nix
+++ control_mode_plugins/default.nix
@@ -3,6 +3,7 @@
 with pkgs;
 
 {
+  empty-control-mode = callPackage ./empty_control_mode { };
   teleop-modular-input-publisher-mode = callPackage ./input_publisher_mode { };
   teleop-modular-twist = callPackage ./teleop_modular_twist { };
 }
